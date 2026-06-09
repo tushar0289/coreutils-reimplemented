a614565b25cb8423dbfbc9739c44b11486830f51
fardin <190056372+tushar0289@users.noreply.github.com>
2026-06-09 22:05:24 +0600

    Add help feature

--- re-cat/re-cat.c
+++ re-cat/re-cat.c
@@ -8,11 +8,19 @@
 typedef struct Arguments {
     int file_count;
     char **files;
+
+    _Bool long_option;
+    _Bool short_option;
+    _Bool line_numbers;
+    _Bool help;
 } Arguments;
 
-void parse_arguments(int argc, char **argv, Arguments *args);
-size_t write_files(const char *path);
+int parse_arguments(int argc, char **argv, Arguments *args);
+size_t write_files(const char *path, Arguments *args);
 size_t write_file_stream(FILE *stream, const char *source_name);
+void parse_long_option(int index, char **argv, Arguments *arg);
+void parse_short_option(int index, char **argv, Arguments *arg);
+void write_help(void);
 
 int main(int argc, char **argv) {
     Arguments args = {0};
@@ -20,12 +28,18 @@ int main(int argc, char **argv) {
     size_t total_size = 0;
 
     parse_arguments(argc, argv, &args);
+
+    if (args.help) {
+        write_help();
+        exit(0);
+    }
+
     if (args.file_count == 0) {
         total_size += write_file_stream(stdin, "stdin");
     } else {
         for (int i = 0; i < args.file_count; i++) {
             putchar('\n');
-            size = write_files(args.files[i]);
+            size = write_files(args.files[i], &args);
 
             if (!size) {
                 continue;
@@ -42,17 +56,25 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void parse_arguments(int argc, char **argv, Arguments *args) {
-    int index = 0;
+int parse_arguments(int argc, char **argv, Arguments *args) {
+    int track_options = 0;
 
-    for (int i = 0; i < argc; i++) {
-        if (strcmp(argv[i], "--help") == 0) {
-            puts("Options will be added later.");
-            exit(0);
+    for (int i = 1; i < argc; i++) {
+        if (strncmp(argv[i], "--", 2) == 0) {
+            args->long_option = 1;
+            parse_long_option(i, argv, args);
+            track_options++;
+        } else if (strncmp(argv[i], "-", 1) == 0) {
+            args->short_option = 1;
+            parse_short_option(i, argv, args);
+            track_options++;
         }
     }
 
-    args->file_count = argc - 1;
+    if (args->help)
+        return 1;
+
+    args->file_count = argc - track_options - 1;
 
     if (args->file_count > 0) {
         args->files = malloc(args->file_count * sizeof(char *));
@@ -61,13 +83,13 @@ void parse_arguments(int argc, char **argv, Arguments *args) {
             exit(1);
         }
 
-        for (int i = 0; i < args->file_count; i++) {
-            args->files[i] = argv[i + 1];
+        for (int i = 0; i < argc; i++) {
+            args->files[i] = argv[i + 1 + track_options];
         }
     }
 }
 
-size_t write_files(const char *path) {
+size_t write_files(const char *path, Arguments *args) {
     size_t read_bytes = 0;
     size_t written = 0;
     size_t size = 0;
@@ -122,3 +144,24 @@ size_t write_file_stream(FILE *stream, const char *source_name) {
 
     return total_written - newline_count;
 }
+
+void parse_long_option(int index, char **argv, Arguments *args) {
+    if ((strcmp(argv[index], "--help")) == 0) {
+        args->help = 1;
+    }
+}
+
+void parse_short_option(int index, char **argv, Arguments *args) {
+    char *options = argv[index];
+    puts(options);
+    printf("%zu\n", strlen(options));
+
+    for (int i = 1; i < strlen(options); i++) {
+        switch (options[1]) {
+        case 'n':
+            args->line_numbers = 1;
+        }
+    }
+}
+
+void write_help(void) { printf("Will help you later :)) \n"); }
