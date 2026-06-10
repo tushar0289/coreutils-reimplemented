d6502aa60c7aab5dd882f394bae1e514898bb5d4
fardin <190056372+tushar0289@users.noreply.github.com>
2026-06-10 18:22:43 +0600

    Add line number flag

--- re-cat/re-cat.c
+++ re-cat/re-cat.c
@@ -16,11 +16,13 @@ typedef struct Arguments {
 } Arguments;
 
 int parse_arguments(int argc, char **argv, Arguments *args);
-size_t write_files(const char *path, Arguments *args);
+size_t process_inputs(char **argv, Arguments *args);
+size_t write_files(const char *path, FILE *file_ptr);
 size_t write_file_stream(FILE *stream, const char *source_name);
 void parse_long_option(int index, char **argv, Arguments *arg);
 void parse_short_option(int index, char **argv, Arguments *arg);
 void write_help(void);
+size_t write_numbered_files(const char *path, FILE *file_ptr);
 
 int main(int argc, char **argv) {
     Arguments args = {0};
@@ -37,20 +39,11 @@ int main(int argc, char **argv) {
     if (args.file_count == 0) {
         total_size += write_file_stream(stdin, "stdin");
     } else {
-        for (int i = 0; i < args.file_count; i++) {
-            putchar('\n');
-            size = write_files(args.files[i], &args);
-
-            if (!size) {
-                continue;
-            }
-
-            printf("\n%zu bytes written from %s\n", size, args.files[i]);
-            total_size += size;
-        }
+        total_size = process_inputs(argv, &args);
     }
 
-    printf("Total bytes written: %zu\n", total_size);
+    if (!args.line_numbers)
+        printf("Total bytes written: %zu\n", total_size);
 
     free(args.files);
     return 0;
@@ -90,38 +83,57 @@ int parse_arguments(int argc, char **argv, Arguments *args) {
     return 1;
 }
 
-size_t write_files(const char *path, Arguments *args) {
+size_t process_inputs(char **argv, Arguments *args) {
+    size_t size = 0;
+    size_t total_size = 0;
+
+    for (int i = 0; i < args->file_count; i++) {
+        FILE *fp = fopen(args->files[i], "rb");
+        if (fp == NULL) {
+            perror(argv[i]);
+            return 0;
+        }
+
+        if (args->line_numbers) {
+            write_numbered_files(argv[i], fp);
+        } else {
+            size = write_files(argv[i], fp);
+            if (!size)
+                continue;
+
+            printf("%zu bytes written from %s\n", size, argv[i]);
+            total_size += size;
+        }
+    }
+    return total_size;
+}
+
+size_t write_files(const char *path, FILE *file_ptr) {
     size_t read_bytes = 0;
     size_t written = 0;
     size_t size = 0;
     char buffer[MAX_LEN];
 
-    FILE *fp = fopen(path, "rb");
-    if (fp == NULL) {
-        perror(path);
-        return 0;
-    }
-
-    while ((read_bytes = fread(buffer, sizeof(char), sizeof(buffer), fp)) !=
-           0) {
+    while ((read_bytes =
+                fread(buffer, sizeof(char), sizeof(buffer), file_ptr)) != 0) {
         written = fwrite(buffer, sizeof(char), read_bytes, stdout);
 
         size += written;
 
         if (read_bytes > written) {
             perror("Error writing to standard output");
-            fclose(fp);
+            fclose(file_ptr);
             return size;
         }
     }
 
-    if (ferror(fp)) {
+    if (ferror(file_ptr)) {
         perror(path);
-        fclose(fp);
+        fclose(file_ptr);
         return 0;
     }
 
-    fclose(fp);
+    fclose(file_ptr);
     return size;
 }
 
@@ -166,3 +178,33 @@ void parse_short_option(int index, char **argv, Arguments *args) {
 }
 
 void write_help(void) { printf("Will help you later :)) \n"); }
+
+size_t write_numbered_files(const char *path, FILE *file_ptr) {
+    size_t line_count = 1;
+    _Bool start_of_line = 1;
+    int c;
+
+    printf("%6zu   ", line_count++);
+
+    while ((c = fgetc(file_ptr)) != EOF) {
+        if (start_of_line) {
+            printf("%7zu   ", line_count++);
+            start_of_line = 0;
+        }
+
+        putchar(c);
+
+        if (c == '\n') {
+            start_of_line = 1;
+        }
+    }
+
+    if (ferror(file_ptr)) {
+        perror(path);
+        fclose(file_ptr);
+        return 0;
+    }
+
+    fclose(file_ptr);
+    return 1;
+}
